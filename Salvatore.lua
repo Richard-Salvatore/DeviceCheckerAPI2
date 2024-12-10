c0f07e0433258608a9e36f040eb3b54b38e95378
Richard-Salvatore <166079004+Richard-Salvatore@users.noreply.github.com>
2024-12-11 00:50:16 +0100

    Create Salvatore.lua

---
+++ Salvatore.lua
@@ -0,0 +1,19 @@
+local api = {}
+
+local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local proj = "Salvatore"
+
+api.log = function(...)
+    
+end
+
+api.warn = function(...)
+    
+end
+
+api.notify = function(emoji, header, content, time)
+   
+end
+
+return api
